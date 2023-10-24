7c26f9425497b0471822feffd432e1be4f93b8cd
RONALD ALEXIS CUADROS BENAVIDES <ronaldcuadros.197@gmail.com>
2023-10-24 12:55:55 -0500

    initial commit

---
+++ controllers/handleCastErrorDB.js
@@ -0,0 +1,7 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message);
+};
+exports.handleCastErrorDB = handleCastErrorDB;
