4185df73ae4a351c24dd16bfc1f9ef12992825c3
RONALD ALEXIS CUADROS BENAVIDES <ronaldcuadros.197@gmail.com>
2023-10-24 13:06:11 -0500

    initial commit

---
+++ public/js/mapbox.js
@@ -0,0 +1,52 @@
+/* eslint-disable */
+
+export const displayMap = locations => {
+  mapboxgl.accessToken = 
+    'pk.eyJ1IjoicmN1YWRyb3MiLCJhIjoiY2xubm4xNmp2MDYyazJrcDF1ZmZqeTFjeiJ9.qnqdmvt7ZnmwLYeWfxs9Fg';
+  
+  var map = new mapboxgl.Map({
+    container: 'map',
+    //   style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/rcuadros/clnnt9b7e007q01pfacca2mq1',
+    scrollZoom: false
+    //   center: [-118.113491, 34.111745],
+    //   zoom: 10,
+    //   interactive: false
+  });
+
+  const bounds = new mapboxgl.LngLatBounds();
+
+  locations.forEach(loc => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
+
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
+
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
+
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+};
